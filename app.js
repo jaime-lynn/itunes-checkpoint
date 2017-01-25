58985764a0878d6c882abf13ccb3eecba37bab9f
jaime-lynn <jaime.madsen@gmail.com>
2017-01-25 12:22:48 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,47 @@
+function ItunesController(){
+  var itunesService = new ItunesService()
+  //Do Not Modify the getMusic function
+  this.getMusic = function getMusic(e){
+    e.preventDefault();
+    var artist = e.target.artist.value;
+    itunesService.getMusicByArtist(artist).then(drawSongs);
+  }
+
+  var musicPreview = function(){
+      var a = document.querySelectorAll('a');
+      audio = document.getElementById('preview');
+      audio.pause();
+      for(var i = 0; i < a.length; i++){
+        a[i].addEventListener('click', function(){
+            var previewURL = this.getAttribute('value');
+            audio.setAttribute('src', previewURL);
+            audio.play();
+        });
+      }
+    }
+
+  function drawSongs(songList){
+    console.log(songList);
+    // This is where you task begins
+    document.getElementById('songs').innerHTML = '';
+    var template = '';
+    for(var i = 0; i < songList.length; i++) {
+        song = songList[i];
+        thumbnail = song.albumArt || 'http://placehold.it/70x70';
+        template += `<tr>
+                        <td><img src="${thumbnail}" class="thumbnail"></td>
+                        <td>${song.artist}</td>
+                        <td><a value="${song.preview}"><span class="glyphicon glyphicon-play" aria-hidden="true"></span> ${song.title}</a></td>
+                        <td>${song.collection}</td>
+                        <td>${song.price}</td>
+                    </tr>`
+    }
+    document.getElementById('songs').innerHTML = template;
+    musicPreview();
+  }
+
+}
+
+
+
+var itunesCtrl = new ItunesController()
